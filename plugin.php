bb8c9d4752a5f8d4d95478fc0ce6f576fff32f7e
LisaCee <lisacanini@sandhillsdev.com>
2021-03-26 15:26:18 -0700

    adding react

--- plugin.php
+++ plugin.php
@@ -9,28 +9,50 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/** Register my blocks through reusable function, rather than
+ * call register_block_type multiple times.
+
+ * @since lesson #26
+ *
+ * @param object $block The slug for the block.
+ * @param array  $options Additional options to add to blocks.
+ * @return void
+ */
+function mytheme_blocks_register_block_type( $block, $options = array() ) {
+	register_block_type(
+		'mytheme-blocks/' . $block,
+		array_merge(
+			array(
+				'editor_script' => 'mytheme-blocks-editor-script',
+				// 'script'        => '',
+				// 'style'         => '',
+				// 'editor_style'  => '',
+			),
+			$options,
+		)
+	);
+}
+
+/** Register my blocks.
+
+ * @since lesson #26
+ *
+ * @return void
+ */
 function mytheme_blocks_register() {
 	wp_register_script(
-		'mytheme-blocks-firstblock-editor-script',
+		'mytheme-blocks-editor-script',
 		plugins_url(
-			'blocks/firstblock/index.js',
+			'dist/editor.js',
 			__FILE__
 		),
 		array(
 			'wp-blocks',
 			'wp-i18n',
-			// 'wp-element',
+			'wp-element',
 		)
 	);
-	register_block_type(
-		'mytheme-blocks/firstblock',
-		array(
-			'editor_script' => 'mytheme-blocks-firstblock-editor-script',
-			// 'script'        => '',
-			// 'style'         => '',
-			// 'editor_style'  => '',
-		)
-	);
+	mytheme_blocks_register_block_type( 'firstblock' );
+	// mytheme_blocks_register_block_type( 'secondblock', array() );
 }
 add_action( 'init', 'mytheme_blocks_register' );
-// add_action( 'enqueue_block_editor_assets', 'mytheme_blocks_register' );
